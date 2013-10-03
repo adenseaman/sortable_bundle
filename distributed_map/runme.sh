686872429fb39e49c5c74a8b29eda215230e8343
Aden Seaman <anseaman@lord.uwaterloo.ca>
2013-10-03 16:49:54 -0400

    Change Erlang cookie.
    Change Erlang cookie name to something more appetizing.

--- distributed_map/runme.sh
+++ distributed_map/runme.sh
@@ -4,7 +4,7 @@ NODELIST="node1@localhost"
 
 echo "Starting Erlang worker nodes"
 for i in $(seq 2 5);do
-  erl -pa ebin -noinput -setcookie crap -sname node${i}@localhost &
+  erl -pa ebin -noinput -setcookie oatmealraisin -sname node${i}@localhost &
   NODELIST="${NODELIST} node${i}@localhost"
 done
 
@@ -15,9 +15,9 @@ echo "type one of the following to begin the demo:"
 echo "  \"distmap:slow_example()\" for a throttled test on a small amount of data"
 echo "  \"distmap:fast_example\" for an unthrottled test on a large amount of data"
 echo "===================="
-erl -pa ebin -setcookie crap -sname node1@localhost -run distmap setupcluster ${NODELIST}
+erl -pa ebin -setcookie oatmealraisin -sname node1@localhost -run distmap setupcluster ${NODELIST}
 
 echo "Killing Erlang worker nodes"
-for PID in $(ps axu | grep "\-noinput \-setcookie crap" | grep -v "grep" | sed "s/^[^\ ]*\ *\([0-9]*\)\ *.*/\1/g"); do
+for PID in $(ps axu | grep "\-noinput \-setcookie oatmealraisin" | grep -v "grep" | sed "s/^[^\ ]*\ *\([0-9]*\)\ *.*/\1/g"); do
   kill -HUP ${PID}
 done
